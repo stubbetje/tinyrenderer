b805da15983976e3b888ea347d463a6c66709bed
Stefan Stubbe <stefan.stubbe@devinity.be>
2016-10-04 21:53:38 +0200

    triangles boundaries

--- go/model/triangle.go
+++ go/model/triangle.go
@@ -12,7 +12,21 @@ func draw_line( v0, v1 Vec2i, img *image.RGBA , c color.Color ) {
 }
 
 func Triangle_first( t0, t1, t2 Vec2i, img *image.RGBA, c color.Color ) {
-	draw_line( t0, t1, img, c )
-	draw_line( t1, t2, img, c )
-	draw_line( t2, t0, img, c )
+	// sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!)
+	if t0.Y > t1.Y {
+		t0, t1 = t1, t0
+	}
+	if t0.Y > t2.Y {
+		t0, t2 = t2, t0
+	}
+	if t1.Y > t2.Y {
+		t1, t2 = t2, t1
+	}
+
+	var red = color.RGBA{0xFF,0,0,0xFF}
+	var green = color.RGBA{0,0xFF,0,0xFF}
+
+	draw_line( t0, t1, img, green )
+	draw_line( t1, t2, img, green )
+	draw_line( t2, t0, img, red )
 }
